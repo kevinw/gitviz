e4a857c09d9441757b78efdb76064aba3cfbc290
kwatters <kwatters@tagged.com>
2012-09-15 13:24:29 -0700

    Fix for showing the "master" branch in empty repositories.
    Also some reorganization, cleanup, and documentation.

--- gitviz.py
+++ gitviz.py
@@ -2,103 +2,90 @@
 Reads the contents of a git repository and write a DOT graph file to stdout.
 '''
 
-import sys
 import dulwich.repo
 import dulwich.index
 import dulwich.objects
 import pydot
 import subprocess
-import tempfile
 
 DEFAULT_FONTNAME = 'Monaco'
 DEFAULT_FONTSIZE = '8'
 
-MAX_BLOB = 200
+BLOB_CONTENT_LIMIT = 200 # show at most this many bytes of blob content
 
-vertices = {}
+DEFAULT_FONT = dict(fontname=DEFAULT_FONTNAME, fontsize=DEFAULT_FONTSIZE)
 
-DEFAULT_FONT = dict(
-    fontname=DEFAULT_FONTNAME,
-    fontsize=DEFAULT_FONTSIZE
-)
 
-def node_opts(**opts):
-    '''Display options for vertices.'''
+def emit_repo_as_xdot(repo, options):
+    '''Emits xdot for the given repo on stdout.'''
 
-    opts.update(DEFAULT_FONT)
-    return opts
+    global graph # TODO: globals are bad mmmmkay
+    global vertices
 
-def edge_opts(**opts):
-    '''Display options for edges.'''
+    vertices = {}
 
-    opts.update(
-        labelfontsize='11',
-        labelfloat="False",
-        **DEFAULT_FONT)
+    graph = pydot.Graph(verbose=True)
+    graph.set_bgcolor('#00000000') # transparent background
 
-    return opts
+    objstore = repo.object_store
+    seen = set()
 
-def q(s):
-    '''pydot seems to not be quoting colons in labels, even though not doing
-    so apparently results in invalid DOT files. quote them here.'''
+    # walk everything in the object store. (this means orphaned nodes will show.)
+    for sha in objstore:
+        if not options.blobs and objstore[sha].type_name in ('blob', 'tree'):
+            continue
+        walk_node(objstore, seen, sha, options)
 
-    return s.replace(':', r'\:')
+    for ref in repo.refs.keys():
+        if ref == 'HEAD': continue # TODO: let this loop handle symbolic refs too
+        branch_node = add_branch_node(ref)
+        graph.add_edge(pydot.Edge(branch_node, repo.refs[ref], **edge_opts(style='dotted')))
 
-def vertex_opts_for_obj(obj, **opts):
-    '''Return pydot display options for a git repository object.'''
+    # do HEAD as a special case
+    ref = 'HEAD'
+    nopts = node_opts(label=ref, shape='diamond', style='filled', fillcolor='#ff3333', fontcolor='white', tooltip='Symbolic Ref: HEAD')
+    head_node = pydot.Node(ref, **nopts)
+    graph.add_node(head_node)
 
-    opts = node_opts(**opts)
+    symref = repo.refs.read_ref(ref)
+    if symref.startswith('ref: '):
+        symref = symref[5:]
+    points_to = add_branch_node(symref)
+    graph.add_node(points_to)
+    graph.add_edge(pydot.Edge(head_node, add_branch_node(symref), **edge_opts(style='dotted')))
 
-    def shortsha():
-        return q(obj.sha().hexdigest()[:20])
+    # index
+    if options.index:
+        try:
+            head_tree = repo['HEAD'].tree
+        except KeyError:
+            head_tree = None
 
-    if obj.type_name == 'commit':
-        opts.update(
-            label=q(obj.message),
-            style='filled',
-            shape='note',
-            fillcolor='#ccffcc',
-            tooltip='Commit: ' + shortsha()
-        )
-    elif obj.type_name == 'tree':
-        opts.update(
-            shape='folder',
-            label='tree',
-            fontcolor='#a0a0a0',
-            style='filled',
-            fillcolor='#ffffff',
-            tooltip='Tree: ' + shortsha()
-        )
-    elif obj.type_name == 'blob':
-        label = q(str(obj).decode('ascii', 'ignore').replace('\0', '').replace('\n', '\\n')[:MAX_BLOB])
-        opts.update(
-            style='filled',
-            fillcolor='#ffffff',
-            shape='ellipse',
-            label=label,
-            tooltip='Blob: ' + shortsha()
-        )
-    else:
-        opts.update(
-            shape='ellipse',
-            label=q(repr(obj)),
-            style='filled',
-            fillcolor='#ffffff'
-        )
+        index = repo.open_index()
 
-    if 'label' in opts:
-        opts['label'] = opts['label'].strip()
+    try:
+        changes = list(index.changes_from_tree(objstore, head_tree))
+    except TypeError:
+        # the official dulwich repo throws a TypeError changes_from_tree is
+        # called against an empty tree (None)
+        if head_tree is not None: raise
+        changes = []
 
-    return opts
+    if changes:
+        index_node = pydot.Node('index', shape='invtriangle', style='filled', fillcolor='#33ff33', fontname=DEFAULT_FONTNAME, fontsize=DEFAULT_FONTSIZE)
+        graph.add_node(index_node)
+        for (oldpath, newpath), (oldmode, newmode), (oldsha, newsha) in changes:
+            graph.add_edge(pydot.Edge(index_node, vert_for_sha(objstore, newsha), label=q('  ' + newpath), fontname=DEFAULT_FONTNAME, fontsize=DEFAULT_FONTSIZE))
 
-graph = pydot.Graph(verbose=True) # TODO: globals are bad mmmmkay
-graph.set_bgcolor('#00000000')
+    # invoke dot -Txdot to turn out DOT file into an xdot file, which canviz is expecting
+    subprocess.Popen(['dot', '-Txdot'], stdin=subprocess.PIPE).communicate(graph.to_string())
 
 def vert_for_sha(objstore, sha, **opts):
     if isinstance(sha, pydot.Node):
         sha = sha.sha
 
     vert = vertices.get(sha)
+
     try:
         obj = objstore[sha]
     except KeyError:
@@ -156,63 +143,92 @@ def walk_node(objstore, seen, sha, options):
             add_edge(vert, parent_vert, weight='%s' % weight)
             walk_node(objstore, seen, parent_sha, options)
 
-def emit_repo_as_xdot(repo, options):
-    '''emit xdot on stdout'''
+def add_branch_node(ref):
+    nopts = node_opts(
+        label=nice_ref_label(ref),
+        shape='diamond',
+        style='filled',
+        tooltip='Branch: %s' % nice_ref_label(ref))
 
-    objstore = repo.object_store
-    seen = set()
+    node = pydot.Node(ref, **nopts)
+    graph.add_node(node)
+    return node
 
-    # walk everything in the object store. (this means orphaned nodes will show.)
-    for sha in objstore:
-        if not options.blobs and objstore[sha].type_name in ('blob', 'tree'):
-            continue
-        walk_node(objstore, seen, sha, options)
+def node_opts(**opts):
+    'Display options for vertices.'
 
-    for ref in repo.refs.keys():
-        if ref == 'HEAD':
-            continue # TODO: let this loop handle symbolic refs too
+    opts.update(DEFAULT_FONT)
+    return opts
 
-        nopts = node_opts(label=nice_ref_label(ref), shape='diamond', style='filled', tooltip='Branch: %s' % nice_ref_label(ref))
-        head_node = pydot.Node(ref, **nopts)
-        graph.add_node(head_node)
-        graph.add_edge(pydot.Edge(head_node, repo.refs[ref], **edge_opts(style='dotted')))
+def edge_opts(**opts):
+    'Display options for edges.'
 
-    # do HEAD as a special case
-    ref = 'HEAD'
-    nopts = node_opts(label=ref, shape='diamond', style='filled', fillcolor='#ff3333', fontcolor='white', tooltip='Symbolic Ref: HEAD')
-    head_node = pydot.Node(ref, **nopts)
-    graph.add_node(head_node)
-    symref = repo.refs.read_ref(ref)
-    if symref.startswith('ref: '):
-        symref = symref[5:]
-    graph.add_edge(pydot.Edge(head_node, symref, **edge_opts(style='dotted')))
+    opts.update(labelfontsize='11', labelfloat="False", **DEFAULT_FONT)
+    return opts
 
-    # index
-    try:
-        head_tree = repo['HEAD'].tree
-    except KeyError:
-        head_tree = None
+def q(s):
+    '''pydot seems to not be quoting colons in labels, even though not doing
+    so apparently results in invalid DOT files. quote them here.'''
 
-    index = repo.open_index()
+    return s.replace(':', r'\:')
 
-    try:
-        changes = list(index.changes_from_tree(objstore, head_tree))
-    except TypeError:
-        # the official dulwich repo throws a TypeError changes_from_tree is
-        # called against an empty tree (None)
-        if head_tree is not None: raise
-        changes = []
+def get_blob_content(obj):
+    "Return the first part of a blob's content for its the label."
 
-    if changes:
-        index_node = pydot.Node('index', shape='invtriangle', style='filled', fillcolor='#33ff33', fontname=DEFAULT_FONTNAME, fontsize=DEFAULT_FONTSIZE)
-        graph.add_node(index_node)
-        for (oldpath, newpath), (oldmode, newmode), (oldsha, newsha) in changes:
-            graph.add_edge(pydot.Edge(index_node, vert_for_sha(objstore, newsha), label=q('  ' + newpath), fontname=DEFAULT_FONTNAME, fontsize=DEFAULT_FONTSIZE))
+    blob_content = str(obj).decode('ascii', 'ignore') # TODO: does utf8 just work?
+    blob_content = blob_content.replace('\0', '').replace('\n', '\\n')
+    return blob_content[:BLOB_CONTENT_LIMIT]
 
-    # invoke dot -Txdot to turn out DOT file into an xdot file, which canviz is expecting
-    subprocess.Popen(['dot', '-Txdot'], stdin=subprocess.PIPE).communicate(graph.to_string())
+def vertex_opts_for_obj(obj, **opts):
+    'Return pydot display options for a git repository object.'
+
+    opts = node_opts(**opts)
+
+    def shortsha():
+        return q(obj.sha().hexdigest()[:20])
+
+    if obj.type_name == 'commit':
+        opts.update(
+            label=q(obj.message),
+            style='filled',
+            shape='note',
+            fillcolor='#ccffcc',
+            tooltip='Commit: ' + shortsha()
+        )
+    elif obj.type_name == 'tree':
+        opts.update(
+            shape='folder',
+            label='tree',
+            fontcolor='#a0a0a0',
+            style='filled',
+            fillcolor='#ffffff',
+            tooltip='Tree: ' + shortsha()
+        )
+    elif obj.type_name == 'blob':
+        label = q(get_blob_content(obj))
+        opts.update(
+            style='filled',
+            fillcolor='#ffffff',
+            shape='ellipse',
+            label=label,
+            tooltip='Blob: ' + shortsha()
+        )
+    else:
+        opts.update(
+            shape='ellipse',
+            label=q(repr(obj)),
+            style='filled',
+            fillcolor='#ffffff'
+        )
+
+    if 'label' in opts:
+        opts['label'] = opts['label'].strip()
+
+    return opts
 
 def nice_ref_label(ref):
+    'Formats a ref to be more readable for the graph.'
+
     if ref.startswith('refs/heads'):
         label = ref[11:]
     elif ref.startswith('refs/remotes'):
@@ -225,13 +241,15 @@ def nice_ref_label(ref):
 def main(repo_dir, options):
     emit_repo_as_xdot(dulwich.repo.Repo(repo_dir), options)
 
-
 if __name__ == '__main__':
     from optparse import OptionParser
     parser = OptionParser()
     parser.add_option("--no-blobs",
                       action="store_false", dest="blobs", default=True,
                       help="don't show blobs")
+    parser.add_option("--no-index",
+                      action="store_false", dest="index", default=True,
+                      help="don't show the index")
     options, args = parser.parse_args()
 
     repo_dir = args[0]
